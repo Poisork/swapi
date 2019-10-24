b4776dad4ddbb77d39ddb896d820a48cb23cf4f9
poisork <poisork@gmail.com>
2019-10-24 17:18:33 +0300

    Updates

---
+++ precache-manifest.82fb2f7c36412e0f92a80b701d44c112.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e30463d9e62c5a69f50f68745c482fc",
+    "url": "/swapi/index.html"
+  },
+  {
+    "revision": "db827e5dcb4f4e01b30c",
+    "url": "/swapi/static/css/4.e1a5265c.chunk.css"
+  },
+  {
+    "revision": "3c0c816c55b0eafacd87",
+    "url": "/swapi/static/css/5.b33f5cd6.chunk.css"
+  },
+  {
+    "revision": "9e1d542107feafcc12ac",
+    "url": "/swapi/static/css/main.ca08962f.chunk.css"
+  },
+  {
+    "revision": "8e4e486222f1976cd0e2",
+    "url": "/swapi/static/js/0.49207a8c.chunk.js"
+  },
+  {
+    "revision": "fc817107d413553c34b8",
+    "url": "/swapi/static/js/3.4757c274.chunk.js"
+  },
+  {
+    "revision": "db827e5dcb4f4e01b30c",
+    "url": "/swapi/static/js/4.c6386896.chunk.js"
+  },
+  {
+    "revision": "3c0c816c55b0eafacd87",
+    "url": "/swapi/static/js/5.f4e7cc03.chunk.js"
+  },
+  {
+    "revision": "38e9a59f4f71821ca3e6",
+    "url": "/swapi/static/js/6.9792c225.chunk.js"
+  },
+  {
+    "revision": "9e1d542107feafcc12ac",
+    "url": "/swapi/static/js/main.b425e858.chunk.js"
+  },
+  {
+    "revision": "b05ec7fb5c156404aa83",
+    "url": "/swapi/static/js/runtime~main.51c88a68.js"
+  },
+  {
+    "revision": "237b2fc04f22a63a9a91ed5bb7c7ef8a",
+    "url": "/swapi/static/media/Eclipse-1s-200px.237b2fc0.svg"
+  }
+]);
