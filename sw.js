5e04dc7f564cbf193a8f7838ac75a4d2d28018e1
poisork <poisork@gmail.com>
2019-10-25 14:44:18 +0300

    Updates

--- sw.js
+++ sw.js
@@ -16,17 +16,21 @@ self.addEventListener("activate", event => {
   });
 
   self.addEventListener('fetch', (event) => {
+    if(event.request.url.indexOf('http') === 0){
+        event.respondWith(
+            caches.match(event.request).then((resp) => {
+              return resp || fetch(event.request).then((response) => {
+                let responseClone = response.clone();
+                caches.open('v1').then((cache) => {
+                  cache.put(event.request, responseClone);
+                });
+        
+                return response;
+              });
+            }).catch(() => {
+              return caches.match('./index.html');
+            })
+          );
+    }
  
- 
-    event.respondWith( 
-      caches.match(event.request).then((resp) => {
-        console.log(resp)
-        return resp || fetch(event.request).then((response) => {
-          return caches.open('v1').then((cache) => {
-            cache.put(event.request, response.clone());
-            return response;
-          });
-        });
-      })
-    );
   });
